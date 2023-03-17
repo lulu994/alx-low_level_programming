72f29a3389ea9e8644677bfae23eb606c4044c59
lulu994 <loubna.neddah@gmail.com>
2023-03-17 12:32:15 +0000

    3. alphABET

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+  *main - Entry point
+  *Print: all the A-Z characters in lowercase
+  *Return: Always 0 (Success)
+  */
+int main(void)
+{
+	int n = 97;
+
+	while (n <= 122)
+	{
+		putchar(n);
+		n++;
+	}
+		putchar('\n');
+	return (0);
+}
