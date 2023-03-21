8ec05e7b4fd8e3a718bce22a1a8e4f0676846c5d
lulu994 <loubna.neddah@gmail.com>
2023-03-21 13:36:55 +0100

    alphabetx10-2

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -6,15 +6,13 @@
   */
 void print_alphabet_x10(void)
 {
-	int i;
-	int l;
+	int count = 0;
+	char letter;
 
-	for (l = 1; i <= 10; i++)
+	while (count++ <= 9)
 	{
-		for (l = 97; l <= 122; l++)
-		{
-			_putchar(l);
+		for (letter = 'a'; letter  <= 'z'; letter++)
+			_putchar(letter);
 	}
-	_putchar('\n');
-	}
+		_putchar('\n');
 }
