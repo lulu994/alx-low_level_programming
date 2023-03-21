600bacf4605a0fe3434691e9cd16ca71129c9d2f
lulu994 <loubna.neddah@gmail.com>
2023-03-21 15:00:51 +0100

    isalpha

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * _isalpha - check for alphabetic character
+ * @c: char is lowecase or uppercase
+ * Return: 1 if c is lowercase or uppercase, otherwise 0
+ */
+int _isalpha(int c)
+{
+	if ((c >= 'a' && c <= 'z') || (c >= 'A' && <= 'Z'))
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
+	_putcher('\n');
+}
