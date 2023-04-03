d38dadbf0b187fce8fc5b54f9c42d9cbed1b9371
lulu994 <loubna.neddah@gmai.com>
2023-04-03 11:54:16 +0200

    strchr

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -9,18 +9,12 @@
   */
 char *_strchr(char *s, char c)
 {
+	int i = 0;
 
-	 while (*s != '\0')
-	 {
-		 if (*s == c)
-		 {
-			 return (s);
-		 }
-		 s++;
-	 }
-	 if (c == '\0')
-	 {
-		 return (s);
-	 }
-	 return NULL;
+	for (; s[i] >= '\0'; i++)
+	{
+		if (s[i] == c)
+			return (&s[i]);
+	}
+	return (0);
 }
