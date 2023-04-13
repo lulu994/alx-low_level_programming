3cb413c26dc49b7ea67804e831ced37062a9b988
lulu994 <loubna.neddah@gmai.com>
2023-04-13 11:40:58 +0200

    calloc

--- 0x0C-more_malloc_free/2-calloc.c
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -36,10 +36,11 @@ void *_calloc(unsigned int nmemb, unsigned int size)
 	}
 	ptr = malloc(size * nmemb);
 
-	 if (ptr == NULL)
-	 {
-		 return (NULL);
-	 }
-	 _memset(ptr, 0, nmemb * size);
-	 return (ptr);
+	if (ptr == NULL)
+	{
+		return (NULL);
+	}
+	_memset(ptr, 0, nmemb * size);
+
+	return (ptr);
 }
