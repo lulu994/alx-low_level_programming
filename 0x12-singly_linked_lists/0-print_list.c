dad07727220fb369995eb470b13e90187bed11bd
lulu994 <loubna.neddah@gmail.com>
2023-04-27 13:04:27 +0200

    0. Print list

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -13,18 +13,16 @@
  */
 size_t print_list(const list_t *h)
 {
-	unsigned int count = 0;
+	size_t s = 0;
 
-	if (h == NULL)
-		return (0);
-	while (h != NULL)
+	while (h)
 	{
-		if (h->str == NULL)
-			printf("[%u] (nil)\n", h->len);
+		if (!h->str)
+			printf("[0] (nil)\n");
 		else
 			printf("[%u] %s\n", h->len, h->str);
-		count += 1;
 		h = h->next;
+		s++;
 	}
-	return (count);
+	return (s);
 }
