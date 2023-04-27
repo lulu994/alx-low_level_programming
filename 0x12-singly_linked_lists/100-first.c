1234be3d9e290b9cd5d7ddaa4906c7eb22ac8673
lulu994 <loubna.neddah@gmail.com>
2023-04-27 14:13:41 +0200

    5. The Hare and the Tortoise

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -1,22 +1,13 @@
 #include <stdio.h>
-/**
-  *Apply the constructor attribute to startupfun()
-  *so that it is executed before main()
-  */
-void startupfun(void) __attribute__ ((constructor));
+
+void first(void) __attribute__ ((constructor));
 
 /**
- *Apply the destructor attribute to cleanupfun()
- *so that it is executed after main()
+ * first - prints a sentence before the main
+ * function is executed
  */
-void cleanupfun(void)__attribute__ ((destructor));
-
-/**
-  *startupfun - This functions print a text
-  *
-  */
-void startupfun(void)
+void first(void)
 {
-	printf("You're beat! and yet, you must allow,");
-	printf("\nI bore my house upon my back!\n");
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
 }
