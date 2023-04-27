0c39404d96529bef96a3b3bed4cb5d0f95cf32dd
lulu994 <loubna.neddah@gmail.com>
2023-04-27 14:48:56 +0200

    0. Print list

--- 0x12-singly_linked_lists/0-print_list.c
+++ 0x12-singly_linked_lists/0-print_list.c
@@ -11,21 +11,18 @@
 
 size_t print_list(const list_t *h)
 {
-	size_t s = 0;
+	unsigned int count = 0;
 
-	 while (h)
-	 {
-		 if (!h->str)
-		 {
-			 printf("[0] (nil)\n");
-		 }
-		 else
-		 {
-			  printf("[%u] %s\n", h->len, h->str);
-		 }
-		  h = h->next;
-		   s++;
-	 }
-
-	 return (s);
+	if (h == NULL)
+		return (0);
+	while (h != NULL)
+	{
+		if (h->str == NULL)
+			printf("[%u] (nil)\n", h->len);
+		else
+			printf("[%u] %s\n", h->len, h->str);
+		count += 1;
+		h = h->next;
+	}
+	return (count);
 }
--- 0x12-singly_linked_lists/lists.h
+++ 0x12-singly_linked_lists/lists.h
@@ -1,6 +1,9 @@
-#ifndef LISTS_H
-#define LISTS_H
-
+#ifndef HEADER_FILE
+#define HEADER_FILE
+#include<string.h>
+#include <stdlib.h>
+#include <stdarg.h>
+#include <stdio.h>
 /**
  * struct list_s - singly linked list
  * @str: string - (malloc'ed string)
@@ -11,9 +14,9 @@
  */
 typedef struct list_s
 {
-    char *str;
-    unsigned int len;
-    struct list_s *next;
+	char *str;
+	unsigned int len;
+	struct list_s *next;
 } list_t;
 
 int _putchar(char c);
@@ -22,5 +25,4 @@ size_t list_len(const list_t *h);
 list_t *add_node(list_t **head, const char *str);
 list_t *add_node_end(list_t **head, const char *str);
 void free_list(list_t *head);
-
 #endif
