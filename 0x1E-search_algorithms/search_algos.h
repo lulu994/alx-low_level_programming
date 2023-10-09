45bd0d3e85f5c55b8da37925df8ac150fb738f35
lulu994 <loubna.neddah@gmail.com>
2023-10-09 13:13:01 +0200

    Linear search in a skip list

--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -1,11 +1,8 @@
-#ifndef HEADER_FILE
-#define HEADER_FILE
+#ifndef SEARCH_ALGOS_H
+#define SEARCH_ALGOS_H
 
-#include<string.h>
-#include <stdlib.h>
-#include <stdarg.h>
 #include <stdio.h>
-#include <math.h>
+#include <stdlib.h>
 
 /**
  * struct listint_s - singly linked list
@@ -15,26 +12,39 @@
  * @next: Pointer to the next node
  *
  * Description: singly linked list node structure
- * for Holberton project
  */
 typedef struct listint_s
 {
-    int n;
-    size_t index;
-    struct listint_s *next;
+	int n;
+	size_t index;
+	struct listint_s *next;
 } listint_t;
-/*-------------Prototype About THe linked List------------------*/
-listint_t *create_list(int *array, size_t size);
-void print_list(const listint_t *list);
-void free_list(listint_t *list);
-listint_t *jump_list(listint_t *list, size_t size, int value);
 
-/* ------------Prototype About The Functions-----------------*/
+/**
+ * struct skiplist_s - Singly linked list with an express lane
+ *
+ * @n: Integer
+ * @index: Index of the node in the list
+ * @next: Pointer to the next node
+ * @express: Pointer to the next node in the express lane
+ *
+ * Description: singly linked list node structure with an express lane
+ */
+typedef struct skiplist_s
+{
+	int n;
+	size_t index;
+	struct skiplist_s *next;
+	struct skiplist_s *express;
+} skiplist_t;
+
 int linear_search(int *array, size_t size, int value);
 int binary_search(int *array, size_t size, int value);
 int jump_search(int *array, size_t size, int value);
 int interpolation_search(int *array, size_t size, int value);
 int exponential_search(int *array, size_t size, int value);
 int advanced_binary(int *array, size_t size, int value);
+listint_t *jump_list(listint_t *list, size_t size, int value);
+skiplist_t *linear_skip(skiplist_t *list, int value);
 
-#endif
+#endif /* SEARCH_ALGOS_H */
